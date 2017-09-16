47641ebdae27b9d521f73d2ea70a6f8321f880af
KevChausse <chaussekevin@gmail.com>
2017-09-16 19:27:49 +0200

    V 0.3.2 - Gestion de l'affichage de la liste des utilisateurs et le détail d'un utilisateur

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,41 @@ var connection = require('../lib/connection');
 
 /* GET user listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
+      var getUsersList = function(retfunc){
+          connection.query('SELECT idext_user, fname_user, lname_user, email_user, birthdate_user FROM user_cashReg', function(error, results, fields) {
+              
+              if(error) res.send(error);
+              else retfunc(results);
+  
+          });
+      }
+  
+      getUsersList(function(results) {
+          res.json(results);
+      });
+  
+});
+
+
+
+/* GET user detail. */
+router.get('/:idext_user', function(req, res, next) {
+    idext_user = req.params.idext_user
+
+    var getUser = function(retfunc){
+        connection.query('SELECT idext_user, fname_user, lname_user, email_user, birthdate_user FROM user_cashReg WHERE idext_user = ?', [idext_user], function(error, results, fields) {        
+            if(error) res.send(error);
+            else {
+                retfunc(results);
+            }
+        });
+    }
+
+    getUser(function(results) {
+        res.json(results);
+    });
+
 });
 
 
